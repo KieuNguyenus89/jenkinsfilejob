44aa73d173b250b81b4ae0f33e2272c355a6e0ce
kieunguyenus89 <kieunguyenus89@gmail.com>
2024-03-03 23:15:00 -0500

    first commit

---
+++ src/main/java/com/example/springbootwithjenkin1/SpringBootWithJenkin1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithjenkin1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJenkin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithJenkin1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootwithjenkin1/SpringBootWithJenkin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithjenkin1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJenkin1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
